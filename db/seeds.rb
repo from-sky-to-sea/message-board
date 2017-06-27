ea4927afc4f80d65b5a692bdb0bd81ffe946f96a
k_ukai <ken151102@gmail.com>
2017-06-27 05:25:02 +0000

    message model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+print 'MessageModel '
+10.times do |n|
+  Message.create!(content: "hello#{n}")
+  print "."
+end
+puts ' ->ok'
